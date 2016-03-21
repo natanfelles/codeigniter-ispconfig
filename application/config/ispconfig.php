6511e447b5dcca36dfd5ceba9755c33af8097cd6
Natan Felles <natanfelles@gmail.com>
2016-03-21 01:12:37 -0300

    Create configuration file

---
+++ application/config/ispconfig.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['username'] = 'admin';
+$config['password'] = 'admin';
+$config['soap_uri'] = 'http://192.168.1.100/remote/';
+$config['soap_location'] = 'http://192.168.1.100/remote/index.php';
+$config['invoices_dir'] = 'invoices';
